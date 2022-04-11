3fc13b5e90584c4bda4e4a7c90c796d2ae509579
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-04-11 11:29:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const slideShow = () => {
+//   const slides = document.getElementById(slidesId);
+  const img = document.querySelectorAll("img");
+  const sliderOneImages = document.querySelector(".sliderOne-images");
+  
+  let imageIndex = 0;
+
+  setInterval(function() {
+    imageIndex += 1;
+    if(imageIndex === img.length){
+            imageIndex = 0
+    }
+    sliderOneImages.style.transform = `translate3d(${imageIndex * -1200}px,0,0)`
+  }, 3000);
+};
+
+slideShow();
